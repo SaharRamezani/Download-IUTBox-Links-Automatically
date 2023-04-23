38275d95fcea2bbb7c5c0e5baf0388e122b3399a
sahar <sahar@kali.sahar>
2023-04-23 05:49:32 -0400

    first commit

---
+++ Download URLs/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using HtmlAgilityPack;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // آدرس فایلی که لینک ها توشه
+        string[] addresses = File.ReadAllLines(@"F:\Uni\ترم 8\شبکه 2\URLs.txt");
+
+        foreach (string address in addresses)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                try
+                {
+                    string htmlCode = webClient.DownloadString(address);
+
+                    HtmlDocument doc = new HtmlDocument();
+                    doc.LoadHtml(htmlCode);
+
+                    HtmlNode movieLink = doc.DocumentNode.SelectSingleNode("//a[contains(@href, '.mp4')]");
+
+                    if (movieLink != null)
+                    {
+                        string movieUrl = movieLink.GetAttributeValue("href", "");
+                        string localFileName = Path.GetFileName(movieUrl);
+
+                        webClient.DownloadFile(movieUrl, localFileName);
+
+                        Console.WriteLine($"Downloaded movie: {localFileName}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No movie link found.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error downloading {address}");
+                }
+            }
+        }
+
+        Console.WriteLine("All movies are downloaded.");
+    }
+}
